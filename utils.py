4a37ff0c3f6bb59c242813ebf0e2fa3db397090e
Dean Price <dean1357price1357@gmail.com>
2022-02-21 18:40:44 -0500

    better filename for error

--- utils.py
+++ utils.py
@@ -281,7 +281,7 @@ def run_battery(opt, ddict, fevals = 1000, trials = 5, dims = "all", benchset = 
         y = min(thisf.outs[:argdict["fevals"]])
         if len(thisf.outs[:argdict["fevals"]]) != argdict["fevals"]:
             print("\n", len(thisf.outs),argdict["fxn_name"], "\n")
-            aeo_log.to_netcdf(str(len(thisf.outs)))
+            aeo_log.to_netcdf(argdict["fxn_name"] + str(len(thisf.outs)))
             raise Exception("-- Error: not enough function evaluations run! This many run: " + str(len(thisf.outs)))
 
         return [y, argdict["fxn_name"], argdict["dims"], argdict["trial"]]
